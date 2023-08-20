a4a820ddf3a1ef84f4ce828f77e660ae334c51f7
Ben Lu <ayroblu@gmail.com>
2023-08-20 01:49:15 +0100

    basic changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Example from '../components/example.svelte';
+</script>
+
+<h1>Temp</h1>
+<Example />
+<a href="/gallery">Gallery</a>
