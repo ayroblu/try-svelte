a4a1f5f0fc341e55d32f2a8e1790f3f4fd619299
Ben Lu <ayroblu@gmail.com>
2023-08-20 23:57:26 +0100

    Add sveltekit pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		VitePWA({
-			// registerType: "autoUpdate",
-			manifest: {
-				name: 'Try Svelte',
-				short_name: 'Try Svelte',
-				theme_color: '#323232',
-				background_color: '#323232',
-				icons: [
-					{
-						src: 'favicons/android-chrome-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: 'favicons/android-chrome-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
-			},
-			// workbox: {
-			//   navigateFallback: null,
-			// },
-			filename: 'sw.ts',
-			srcDir: 'src/service-worker',
-			strategies: 'injectManifest'
-			// uses add event listener load - more complexity?
-			// injectRegister: "inline",
-			// devOptions: {
-			//   enabled: true,
-			//   type: "module",
-			// },
-		})
-	],
+	plugins: [sveltekit(), SvelteKitPWA()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
